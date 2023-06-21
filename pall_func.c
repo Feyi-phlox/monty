063fbdfc0d3d6f4870b1e62e0bed38963f8c893d
Feyi-phlox <feyisayoakinbobola@gmail.com>
2023-06-21 14:15:06 -0700

    updated files

--- pall_func.c
+++ pall_func.c
@@ -9,8 +9,10 @@
 
 void pall(stack_t **stack, unsigned int line_number)
 {
+	stack_t *current;
 	(void)line_number;
-	stack_t *current = *stack;
+
+	current = *stack;
 
 	while (current)
 	{
--- push_func.c
+++ push_func.c
@@ -8,33 +8,40 @@
  */
 void push(stack_t **stack, unsigned int line_number)
 {
+	stack_t *new_node;
+	char argument[3];
+	int value;
+
+	value = atoi(argument);
+	new_node = malloc(sizeof(stack_t));
+
 	if (!stack || !line_number)
 	{
 		fprintf(stderr, "L%d: usage: push integer\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
-	stack_t *new_node = malloc(sizeof(stack_t));
-	
 	if (!new_node)
 	{
 		fprintf(stderr, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 	}
-	int value = atoi(stack[1]->opcode);
 
-	if (!value && strcmp(stack[1]->opcode, "0") != 0)
+	if (!value)
 	{
 		fprintf(stderr, "L%d: usage: push integer\n", line_number);
 		free(new_node);
 		exit(EXIT_FAILURE);
 	}
-	
+
 	new_node->n = value;
 	new_node->prev = NULL;
-	new_node->next = *stack;
+	new_node->next = NULL;
 
 	if (*stack)
+	{
+		new_node->next = *stack;
 		(*stack)->prev = new_node;
+	}
 	*stack = new_node;
 }
