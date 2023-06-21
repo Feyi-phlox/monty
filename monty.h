fc3867ba453dc9da48e65d3b91cb38edea835ca8
Feyi-phlox <feyisayoakinbobola@gmail.com>
2023-06-21 14:09:20 -0700

    updated header

--- monty.h
+++ monty.h
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <string.h>
 
+
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -36,6 +37,8 @@ typedef struct instruction_s
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
+void execute_file(char *file_path);
+void process(char *opcode, stack_t **stack, unsigned int line_number);
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 
